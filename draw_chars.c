ad5ec152bfda2f0035ee066f9d50535af1b9a48e
Joel Ramos <jaramos8@miners.utep.edu>
2021-02-18 14:46:58 -0700

    Correct Orientation

--- draw_chars.c
+++ draw_chars.c
@@ -4,10 +4,10 @@
 void print_char_11x16(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 11; row++) {
-    unsigned short rowBits = font_11x16[c][row];
     for (char col = 0; col < 16; col++) {
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+      for (char row = 0; row < 11; row++) {
+       unsigned short rowBits = font_11x16[c][row];
+       unsigned short colMask = 1 << (col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
@@ -17,12 +17,14 @@ void print_char_11x16(char c)
 void print_char_5x7(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 5; row++) {
-    unsigned char rowBits = font_5x7[c][row];
-    for (char col = 0; col < 7; col++) {
-      unsigned char colMask = 1 << (6-col); /* mask to select bit associated with bit */
+
+  for (char col = 0; col < 7; col++) {
+       for (char row = 0; row < 5; row++) {
+      unsigned char rowBits = font_5x7[c][row];
+      unsigned char colMask = 1 << (col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
   }
 }
+
